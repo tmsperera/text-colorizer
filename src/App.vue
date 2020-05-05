22845bfd60b51d4ebfbde9db8d50f9883f28364c
TMS Perera <tmsperera@gmail.com>
2020-05-05 21:38:27 +0530

    Implement functionality
      - Added tailwind CSS framework
      - Added invert-color lib
      - Added axios
      - Implemented App.vue
      - Implemented form
      - Implemented submit button
      - Implemented card adding functionality
      - Implemented data fetching
      - Implemented title updating functionality
      - Set autocomplete off
      - Updated styles

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,145 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+  <div
+    v-cloak
+    class="flex flex-col items-center justify-center min-h-screen p-10"
+  >
+    <div class="flex flex-col w-full md:w-2/3">
+      <h1 class="text-4xl font-hairline text-purple-600 text-center">
+        Colorize your text
+      </h1>
+
+      <form @submit.prevent="addCard" class="mt-3 md:mt-4">
+        <div class="w-full flex flex-col items-center justify-between">
+          <input
+            v-model="text"
+            type="text"
+            ref="text"
+            autofocus
+            autocomplete="off"
+            class="w-full md:w-3/5 py-3 px-2 text-center rounded-lg hover:bg-gray-100 focus:outline-none focus:shadow-inner focus:bg-gray-100 placeholder-current focus:placeholder-gray-400 text-purple-600"
+            :placeholder="
+              cards.length
+                ? 'Add your new text here to...'
+                : 'Add your text here to...'
+            "
+          />
+
+          <button
+            type="submit"
+            :disabled="!valid"
+            :class="
+              valid
+                ? ['text-purple-600', 'shadow-md', 'border']
+                : ['cursor-not-allowed', 'text-gray-500', 'border-none']
+            "
+            class="w-4/5 md:w-40 mt-3 md:mt-4 py-2 px-6 rounded-md focus:outline-none focus:border-purple-500 bg-white"
+          >
+            <div class="flex items-center justify-center">
+              {{ loading ? "Colorizing..." : "Colorize" }}
+            </div>
+          </button>
+        </div>
+      </form>
+
+      <div class="flex flex-col items-center justify-center mt-6">
+        <card
+          v-for="(card, index) in reversedCards"
+          :key="index"
+          :card="card"
+          class="w-full md:w-auto max-w-full"
+        ></card>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import axios from "axios";
+import Card from "./components/Card";
+import invert from "invert-color";
 
 export default {
   name: "App",
-  components: {
-    HelloWorld
+
+  data: function() {
+    return {
+      text: "",
+      cards: [],
+      loading: false
+    };
+  },
+
+  components: { Card },
+
+  computed: {
+    valid() {
+      return !!this.text.trim();
+    },
+
+    reversedCards() {
+      return [...this.cards].reverse();
+    }
+  },
+
+  methods: {
+    async addCard() {
+      if (this.loading) {
+        return;
+      }
+
+      this.loading = true;
+
+      this.cards.push(await this.createCard(this.text));
+
+      this.updateTitle(this.text);
+
+      this.reset();
+
+      this.loading = false;
+    },
+
+    async createCard(text) {
+      let backgroundColor = "#6d4298";
+      let textColor = "#FFFFFF";
+
+      try {
+        const { data: fetched } = await this.fetchColor();
+
+        backgroundColor = fetched.color;
+        textColor = invert(fetched.color);
+      } catch (e) {
+        console.log(
+          "Colorizing server did not responded. Colors set to defaults."
+        );
+      }
+
+      return {
+        text: text.trim(),
+        textColor: textColor,
+        backgroundColor: backgroundColor
+      };
+    },
+
+    updateTitle(text) {
+      document.title = text;
+    },
+
+    fetchColor() {
+      return axios.get("http://api.creativehandles.com/getRandomColor");
+    },
+
+    reset() {
+      this.text = "";
+      this.$refs.text.focus();
+    }
   }
 };
 </script>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+[v-cloak] {
+  display: none;
 }
+
+@import "assets/scss/main";
 </style>
---
+++ src/components/Card.vue
@@ -0,0 +1,18 @@
+<template>
+  <div
+    class="flex items-center justify-center rounded-lg shadow-md px-8 py-8 md:px-16 my-3 text-xl md:text-2xl text-center"
+    :style="{ background: card.backgroundColor, color: card.textColor }"
+  >
+    <p class="max-w-full break-words">
+      {{ card.text }}
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Card",
+
+  props: { card: Object }
+};
+</script>
